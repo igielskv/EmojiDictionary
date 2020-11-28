e3cbce52b1f9baaffc6776410986b32982ff1d7f
Vladislav Igielski <igielskv@icloud.com>
2020-11-28 22:06:59 +0100

    Initial Commit

---
+++ Shared/EmojiDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiDictionaryApp.swift
+//  Shared
+//
+//  Created by Manolis on 28.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct EmojiDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
